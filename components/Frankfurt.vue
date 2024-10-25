79e3d445c6fb2af201b7402e0c525501bfa4dcba
IcathianRain <2205794866@qq.com>
2024-10-25 23:58:27 +0800

    ✨ feat(Franfurt): Modify the header
    Modify the H1 style to header.
    I think the H1 style and content should match the section title.
    So I change It to header, each slide have a header and match the section

--- components/Frankfurt.vue
+++ components/Frankfurt.vue
@@ -1,14 +1,18 @@
 <template>
-	<header class="absolute top-0 left-0 right-0 flex shadow-md shadow-black text-sm px-4 -mx-4" style="background:black">
-		<div v-for="(sec, i) in sections" class="flex-1 py-1 px-3"
+	<header class="absolute top-0 left-0 right-0" >
+        <div class="flex shadow-black shadow-md text-sm px-4 -mx-4 z-2" style="background:black">
+            <div v-for="(sec, i) in sections" class="flex-1 py-1 px-3"
 			 :class="sec[1].includes($slidev.nav.currentPage - 1) ? 'text-white' : 'text-stone'" :key="i">
-			<div>{{ sec[0] || "&emsp;" }}</div>
-			<div>
-				<span v-for="p in sec[1]" :key="p">
-					<Link :to="p + 1" style="color:unset">{{ $slidev.nav.currentPage == p + 1 ? "●" : "○" }}</Link>
-				</span>
-			</div>
-		</div>
+                <div>{{ sec[0] || "&emsp;" }}</div>
+                <div>
+                    <span v-for="p in sec[1]" :key="p">
+                        <Link :to="p + 1" style="color:unset">{{ $slidev.nav.currentPage == p + 1 ? "●" : "○" }}</Link>
+                    </span>
+                </div>
+            </div>
+        </div>
+		
+        <h1> {{ pageSections[$slidev.nav.currentPage-2] }}</h1>
 	</header>
 </template>
 
@@ -34,4 +38,32 @@
 		result.push([title, pages]);
 		return result;
 	});
+
+    const pageSections = computed(()=> {
+        const result: string[] = [];
+        let title = "";
+        for(let i = 1; i < $slidev.nav.slides.length; i++) {
+			const slide = $slidev.nav.slides[i];
+			const section = (slide.meta?.slide as SlideInfoBase)?.frontmatter?.section;
+			if(section && section != title) {
+				title = section;
+			}
+            result.push(title);
+		}
+        return result;
+    })
 </script>
+
+<style scoped>
+h1 {
+    @apply text-white shadow-md;
+    background: #3333B3;
+    position: relative;
+    font-size: xx-large;
+    margin-left: -5px;
+    padding: 0rem 0rem 5px 25px;
+    width: 1000%;
+    --un-shadow-opacity: 1;
+    --un-shadow-color: #3333B3;
+}
+</style>
